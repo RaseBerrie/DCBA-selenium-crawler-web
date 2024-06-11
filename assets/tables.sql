754cd79d7fafb69eb2cf0a36ac7c3d380e965719
김유진 <yj.kim@itforest.net>
2024-06-11 12:56:41 +0900

    database changed

--- assets/tables.sql
+++ assets/tables.sql
@@ -1,11 +1,16 @@
 ### 검색 결과 테이블
-CREATE TABLE searchResult (se CHAR(1) not null,
-                           subdomain CHAR(30) not null,
-                           title VARCHAR(100) not null,
-                           url VARCHAR(500) not null,
-                           content text);
+CREATE TABLE searchResult (
+    se CHAR(1) NOT NULL,
+    subdomain CHAR(30) NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    url VARCHAR(500) NOT NULL,
+    content TEXT,
+    CONSTRAINT unique_se_url UNIQUE (se, url)
+);
 
 ### 검색 키(작업 완료 URL 리스트)테이블
-CREATE TABLE IF NOT EXISTS searchKeys (search_key CHAR(30) NOT NULL UNIQUE,
-                                       google CHAR(1) NOT NULL DEFAULT "N",
-                                       bing CHAR(1) NOT NULL DEFAULT "N");
+CREATE TABLE IF NOT EXISTS searchKeys (
+    search_key CHAR(30) NOT NULL UNIQUE,
+    google CHAR(1) NOT NULL DEFAULT "N",
+    bing CHAR(1) NOT NULL DEFAULT "N"
+);
